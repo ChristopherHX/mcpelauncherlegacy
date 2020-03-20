112048d6866e5d71feaf77ecd22d715b23ba175c
Christopher Homberger <christopher.homberger@web.de>
2020-03-20 23:49:18 +0100

    Fix side effects

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@ extern "C" void mod_init() {
     auto mc = dlopen("libminecraftpe.so", RTLD_LAZY);
     Log::info("MINECRAFT FOUND AT ", "%d", (int)mc);
     Log::info("LOWMEM", "Patch vtable");
-    LauncherAppPlatform::initVtable(mc);
     ClientAppPlatform::initVtable(mc);
     Log::info("LOWMEM", "Vtable patched");
 }
